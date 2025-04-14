0092b961017c22e8386ecbf18b582265f031752c
Krzysiek Durski <dursky.k@gmail.com>
2025-04-14 23:37:01 +0200

    Add css

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)   
+
+app.config.from_object('config')
+url_for('static', filename='css/main.css')
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
