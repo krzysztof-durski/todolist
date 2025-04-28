3f93e615fea5b6ec5c2f309bba5c883d83eaf1db
Krzysiek Durski <dursky.k@gmail.com>
2025-04-28 12:45:03 +0200

    ?

--- app/config.py
+++ app/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = os.getenv(
-    "DATABASE_URL", "postgresql://postgres:password@localhost:5432/todo_db"
+    "DATABASE_URL"
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
