f7e22b79eb50d5979c68d8febeb4bbbe76a33ec0
Krzysiek Durski <dursky.k@gmail.com>
2025-04-28 15:16:13 +0200

    changes in config

--- app/config.py
+++ app/config.py
@@ -7,4 +7,8 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = os.getenv(
     "DATABASE_URL", "postgresql://postgres:password@localhost:5432/todo_db"
 )
+
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
