34596348ed5130720f6ea554cce6d678783a1862
Krzysiek Durski <dursky.k@gmail.com>
2025-04-29 13:28:46 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -6,10 +6,11 @@ load_dotenv()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+#for local development
 if not DATABASE_URL:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, '..', 'todo.db')}"
+    SQLALCHEMY_DATABASE_URI = f"sqliteeee:///{os.path.join(BASE_DIR, '..', 'todo.db')}"
 else:
+#for production
     SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
